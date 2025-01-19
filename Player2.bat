40ee73935e5f4ca5bebb9ef6077d75671e2db7bc
Mahmoud <88500648+mabboud1994@users.noreply.github.com>
2025-01-19 03:12:40 +0100

    wip

--- Player2.bat
+++ Player2.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" "D:\EasyOSSTutorial\EasyOSS\EasyOSSTutorial.uproject" Startup -game -WINDOWED -ResY=450 -ResX=450 -epicapp="ClientDev" AUTH_TYPE="developer" -AUTH_LOGIN="localhost:8081" -AUTH_PASSWORD="EasyOSS_Player2"
+"C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" "D:\EasyOSSTutorial\EasyOSS-EOS\EasyOSSTutorial.uproject" Startup -game -WINDOWED -ResY=450 -ResX=450 -epicapp="ClientDev" AUTH_TYPE="developer" -AUTH_LOGIN="localhost:8081" -AUTH_PASSWORD="EasyOSS_Player2"
